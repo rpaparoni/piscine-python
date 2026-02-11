d6a2dc447738b04595d91d965f525e08051fe04f
Ricardo <paparoniricardo@gmail.com>
2026-02-11 15:00:11 +0100

    more

--- module00/ex4/ft_water_reminder.py
+++ module00/ex4/ft_water_reminder.py
@@ -2,6 +2,6 @@ def ft_water_reminder():
     days = input("Days since last watering: ")
     days = int(days)
     if (days > 2):
-        print("Plants are fine")
-    else:
         print("Plant is ready to harvest!")
+    else:
+        print("Plants are fine")
--- module00/ex6/ft_garden_summary.py
+++ module00/ex6/ft_garden_summary.py
@@ -2,4 +2,4 @@ def ft_garden_summary():
     """Display a garden information"""
     name = input("Enter garden name: ")
     num = input("Enter number of plants: ")
-    print("Garden:",name,"\nPlants:",num,"\nStatus: Growing well!")
+    print(f"Garden: {name}\nPlants: {num}\nStatus: Growing well!")
