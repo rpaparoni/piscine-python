039142675594f116086dd3d9bdbc16807125ddbf
rpaparoni <paparoniricardo@gmail.com>
2026-02-26 19:46:47 +0100

    xd

--- module00/ex3/ft_plant_age.py
+++ module00/ex3/ft_plant_age.py
@@ -1,7 +1,7 @@
 def ft_plant_age():
     days = input("Enter plant age in days: ")
     days = int(days)
-    if (days >= 60):
+    if (days > 60):
         print("Plant is ready to harvest!")
     else:
         print("Plant needs more time to grow.")
--- module00/ex4/ft_water_reminder.py
+++ module00/ex4/ft_water_reminder.py
@@ -2,6 +2,6 @@ def ft_water_reminder():
     days = input("Days since last watering: ")
     days = int(days)
     if (days > 2):
-        print("Plant is ready to harvest!")
+        print("Water the plants!")
     else:
         print("Plants are fine")
