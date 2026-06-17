6d061bf9c60d6b76890e9994b16763781a60ac34
rpaparoni <paparoniricardo@gmail.com>
2026-06-17 02:07:08 +0200

    new

---
+++ module04/ex1/ft_ancient_text.py
@@ -0,0 +1,32 @@
+import sys
+
+
+def read_ancient_fragment(filename: str) -> None:
+    print("=== Cyber Archives Recovery ===")
+    print(f"Accessing file '{filename}'")
+    
+    try:
+        file_to_read = open(filename, "r")
+        print("---\n")
+        
+        text = file_to_read.read()
+        print(f"{text}")
+        
+        file_to_read.close()
+        print("\n---")
+        print(f"File '{filename}' closed.")
+        
+    except Exception as error:
+        print(f"Error opening file '{filename}': {error}")
+
+
+def main() -> None:
+    if len(sys.argv) == 2:
+        file_argument: str = sys.argv[1]
+        read_ancient_fragment(file_argument)
+    else:
+        print("Usage: python3 ft_ancient_text.py <file>")
+
+
+if __name__ == "__main__":
+    main()
